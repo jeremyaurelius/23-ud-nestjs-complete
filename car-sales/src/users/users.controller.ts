657557dd02d226916983242bff2aaefdb29225aa
Jeremy L <mail.jeremy.li@gmail.com>
2023-10-16 16:32:36 +1100

    added route to create user

--- car-sales/src/users/users.controller.ts
+++ car-sales/src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+
+  constructor(
+    private usersService: UsersService
+  ) {}
+
+  @Post()
+  async createUser(@Body() body: CreateUserDto): Promise<User> {
+    // TODO:
+    console.log('body', body);
+    const user = await this.usersService.create(body);
+    console.log('user', user);
+    return user;
+  }
+}
--- car-sales/src/users/users.service.ts
+++ car-sales/src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    // inject a repository based on the User entity
+    // The reason we need the @InjectRepository(User) decorator is because generics are not added in TS metadata
+    @InjectRepository(User) private repo: Repository<User>,
+  ) {}
+
+  /**
+   * Creates a user.
+   */
+  create(userDto: CreateUserDto) {
+    // create - creates an instance of an entity but does not persist it to the DB
+    // save - adds/updates record to DB
+    const user = this.repo.create(userDto);
+    return this.repo.save(user);
+  }
+}
